ac80ed50140f3f59ebc8da297513cafef3d64467
lucasmanrick <112766634+lucasmanrick@users.noreply.github.com>
2023-03-13 15:18:51 -0300

    Melhoria a calculadora
    corrigindo input, para evitar problemas para com o eval e restringindo algumas ações/permissões do usuario

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const inputResult = document.getElementById(`area-result`)
-const allowedArray = [9,8,7,6,5,4,3,2,1,0,`.`,`/`,`*`,`+`,`-`] 
+const inputResult = document.querySelector(`#area-result`)
+const allowedArray = [`9`,`8`,`7`,`6`,`5`,`4`,`3`,`2`,`1`,`0`,`.`,`/`,`*`,`+`,`-`] 
 
+inputResult.addEventListener (`keydown`, function (ev) {
+  ev.preventDefault()
+
+  if (allowedArray.includes(ev.key)) {
+    inputResult.value += ev.key
+    return
+  }
+  if (ev.key === `Backspace`) {
+      inputResult.value =  inputResult.value.slice(0,-1)
+      return
+  }
+})
 
 function cleaningUp () {
   inputResult.value = ""
