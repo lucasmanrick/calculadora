05a1a933f70a601e0e1510386094123e423a027a
lucasmanrick <112766634+lucasmanrick@users.noreply.github.com>
2023-03-13 12:12:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const inputResult = document.getElementById(`area-result`)
+const allowedArray = [9,8,7,6,5,4,3,2,1,0,`.`,`/`,`*`,`+`,`-`] 
+
 
 function cleaningUp () {
   inputResult.value = ""
+  inputResult.focus()
 }
 function addNumbers (number) {
   inputResult.value += `${number}`
+  inputResult.focus()
 }
 
 function operators (operator) {
   inputResult.value += `${operator}`
+  inputResult.focus()
 }
 
 function result () {
   let doCalc = eval (inputResult.value) 
     inputResult.value = doCalc 
+    inputResult.focus()
 }
 
